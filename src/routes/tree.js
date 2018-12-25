124d5f812be8d05a09e3e64f6fa4e696186405f3
Harvy <harvyestebanml@gmail.com>
2018-12-25 02:45:25 -0500

    Showing Each Tree and starting with the meta tags

--- src/routes/tree.js
+++ src/routes/tree.js
@@ -4,18 +4,19 @@ import firebase from 'firebase'
 
 class Tree extends Component{
 	componentWillMount(){
-		/*firebase.database().ref('trees').on('value', snap => {
-			snap.forEach(k => {
+		firebase.database().ref('trees').on('value', snap => {
+			snap.forEach(snapchild => {
 				this.setState({
-					treeCode: this.state.treeCode.concat(snap.val()[k].code)
+					treeCode: this.state.treeCode.concat(snapchild.val())
 				})		
 			})
-		})*/
+		})
 	}
 	constructor(){
 		super()
 		this.state = {
-			treeCode: '0002'
+			treeCode: [],
+			metaTags: []//To put them in the head, so that the SEO improves
 		}
 		this.verify = this.verify.bind(this)
 	}
@@ -23,9 +24,24 @@ class Tree extends Component{
 
 	verify(){
 		const { match, location, history } = this.props;
-		var tree = (this.state.treeCode === match.params.code) ? <h1>{match.params.code}</h1> : 'something went wrong' 
-		console.log(tree)
-
+		const tree = this.state.treeCode.map(tc => (
+				(tc.code === match.params.code) ? <div className="card">
+								                <div className="card-image waves-effect waves-block waves-light">
+								                  <img className="activator" src={tc.img}/>
+								                </div>
+								                <div className="card-content">
+								                  <span className="card-title activator grey-text text-darken-4">{tc.name}<i className="material-icons bottom-right">more_vert</i></span>
+								                  <p><a href="#" className="btn hoverable waves white green-text">Enviar</a></p>
+								                </div>
+								                <div className="card-reveal">
+								                  <span className="card-title grey-text text-darken-4">{tc.name}<i className="material-icons top-right">close</i></span>
+								                  <p>{tc.description}</p>
+								                  <p><a href="#" className="btn hoverable waves-light white green-text">Enviar</a></p>
+								                </div>
+								              </div> : ""
+			)
+		)
+		return tree
 	}
 	render(){
 		const { match, location, history } = this.props;
@@ -38,7 +54,10 @@ class Tree extends Component{
 			)
 		}*/
 		return(
-			<h1>{match.params.code} {this.verify()}</h1>
+			<div className="container">
+				<h1>{this.verify()}</h1>
+			</div>
+			
 
 		)
 
